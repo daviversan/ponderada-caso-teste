cca42578d3fa0bd86444041abfd1d07810546f29
theVersan <158575755+daviversan@users.noreply.github.com>
2025-03-13 18:39:30 -0300

    adiciona arquivo app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+def nome_valido(nome):
+    return all(c.isaplha() or c.isspace() for c in nome)
+
+#Essa função percorre cada caractere da string nome e verifica se é uma letra ou espaço,
+#bloqueando qualquer outro tipo de dado
